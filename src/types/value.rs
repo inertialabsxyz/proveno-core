b4b5c712277f4e0d296571ef3924b37b30dc8add
Andy <andyjsbell@gmail.com>
2026-02-20 16:12:26 +0100

    feat: LuaString impl

--- src/types/value.rs
+++ src/types/value.rs
@@ -6,6 +6,28 @@ pub const MAX_TABLE_ENTRIES: usize = 50_000;
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct LuaString(Arc<[u8]>);
 
+impl LuaString {
+    fn from_bytes(bytes: &[u8]) -> Self {
+        LuaString(Arc::from(bytes))
+    }
+
+    fn from_str(s: &str) -> Self {
+        LuaString(Arc::from(s.as_bytes()))
+    }
+
+    fn as_bytes(&self) -> &[u8] {
+        &self.0
+    }
+
+    fn len(&self) -> usize {
+        self.0.len()
+    }
+
+    fn is_empty(&self) -> bool {
+        self.0.is_empty()
+    }
+}
+
 #[derive(Debug, Clone)]
 pub struct LuaFunction;
 
